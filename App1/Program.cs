aed47b966d50936a3c588630f984cb19470ff402
Krzysztof Krzyszkowski <krzyszkowski@outlook.com>
2022-12-19 11:47:29 +0100

    comment

--- App1/Program.cs
+++ App1/Program.cs
@@ -8,14 +8,13 @@ namespace FunctionApp1
 {
     public class Program
     {
-        const string AiConnectionString = "AI_CONNECTION_STRING_HERE";
+        const string AiConnectionString = "AI_CONNECTION_STRING_HERE"; // in real it would be retrieved from ACC
 
         public static void Main()
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults((workerApplication) =>
                 {
-
                     workerApplication.AddApplicationInsights(o => o.ConnectionString = AiConnectionString);
                     workerApplication.AddApplicationInsightsLogger();
                 })
